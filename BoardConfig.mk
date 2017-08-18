b981877edac421d55ed842de7066e13d2a5d11b9
BitO BSessiOn <bitobsession@gmail.com>
2017-08-18 15:16:33 +0200

    multirom: Param revision, update
    - mr_hooks updated
    - Versioning added (thx no @nkk71)
    - kernel config updated (lineageos)
    - Other minor changes and updates

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 -
 #TARGET_PREBUILT_KERNEL := device/zuk/ham/kernel
 # else uncomment below to build from sauce
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8974
-TARGET_KERNEL_CONFIG := cyanogenmod_k9_defconfig
+TARGET_KERNEL_CONFIG := lineageos_k9_defconfig
 
 # Filesystem
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 20971520
@@ -61,9 +61,23 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_VARIANT := twrp
 
+TW_INCLUDE_NTFS_3G := true
+
+# Asian region languages
+#TW_EXTRA_LANGUAGES := true
+
+
 #MultiROM config. MultiROM also uses parts of TWRP config
 TARGET_RECOVERY_IS_MULTIROM := true
-MR_INPUT_TYPE := type_a
+MR_NO_KEXEC := 2
+# possible options:
+#       1 true allowed      # NO_KEXEC_DISABLED =  0x00,   // no-kexec is disabled (ie it is built, but needs to be manually enabled)
+#       2 enabled           # NO_KEXEC_ALLOWED  =  0x01,   // "Use no-kexec only when needed"
+#       3 ui_confirm        # NO_KEXEC_CONFIRM  =  0x02,   // "..... but also ask for confirmation"
+#       4 ui_choice         # NO_KEXEC_CHOICE   =  0x04,   // "Ask whether to kexec or use no-kexec"
+#       5 forced            # NO_KEXEC_FORCED   =  0x08,   // "Always force using no-kexec workaround"
+# any other setting won't build it at all
+
 MR_INIT_DEVICES := device/zuk/ham/multirom/mr_init_devices.c
 MR_DPI := xhdpi
 MR_DPI_FONT := 340
@@ -74,12 +88,26 @@ MR_USE_MROM_FSTAB := true
 MR_KEXEC_DTB := true
 #MR_INFOS := device/zuk/ham/multirom/mrom_infos
 MR_DEVICE_HOOKS := device/zuk/ham/multirom/mr_hooks.c
-MR_DEVICE_HOOKS_VER := 4
+MR_DEVICE_HOOKS_VER := 6
+
+MR_INPUT_TYPE := type_a
+MR_PIXEL_FORMAT := "RGBX_8888"
 MR_CONTINUOUS_FB_UPDATE := true
 MR_USE_QCOM_OVERLAY := true
 MR_QCOM_OVERLAY_HEADER := device/zuk/ham/include/framebuffer_qcom_overlay.h
 MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
-MR_PIXEL_FORMAT := "RGBX_8888"
+
 MR_DEVICE_VARIANTS := Z1 K9
-MR_ENCRYPTION := true
+#MR_ENCRYPTION := true
+
+# Versioning
+TW_DEVICE_VERSION := 1
+
+include device/zuk/ham/multirom/version/MR_REC_VERSION.mk
+
+ifeq ($(MR_REC_VERSION),)
+MR_REC_VERSION := $(shell date -u +%Y%m%d)-01
+endif
+
+BOARD_MKBOOTIMG_ARGS += --board mrom$(MR_REC_VERSION)
 
